c9f9cbe88c8f4993d8085085f2044cdc6991c870
amina <amina.bouchekhchoukh@gmail.com>
2023-03-13 10:55:45 +0100

    OVERRIDE THE WRITE METHOD (RECIEVE)

--- models/ws_product.py
+++ models/ws_product.py
@@ -44,6 +44,24 @@ class Product(models.Model):
                 vals['brand'] = self.create_brand(vals['brand']).id
                 vals['categ_id'] = self.create_category(vals['categ_id']).id
                 vals['company_id'] = request.env['res.company'].search([('name', '=', vals['company_id'])]).id
+
+                attributes = []
+                for attr in vals['attribute']:
+                    attribute_items = [0, 0]
+                    attr_id = self.create_attribute(attr['attribute_id']).id
+                    values_list = []
+                    for value in attr['value_ids']:
+                        attr_value_id = self.create_attribute_value(value, attr_id).id
+                        values_list.append(attr_value_id)
+                    print(values_list)
+                    attribute_items.append({
+                        'attribute_id': attr_id,
+                        'value_ids': values_list
+                    })
+                    attributes.append(attribute_items)
+                vals['attribute_line_ids'] = attributes
+                vals.pop('attribute')
+
             return super(Product, self).create(vals)
 
         # 2- CREATE A PRODUCT FROM ODOO (Send a product to WS)
@@ -104,6 +122,93 @@ class Product(models.Model):
 
             return super(Product, self).create(vals)
 
+    # UPDATE A PRODUCT
+    def write(self, vals):
+        # THIS IS TO PREVENT "CREATE" FROM CALLING THE "WRITE" METHOD
+        if len(vals.keys()) <= 2 and ('barcode' in vals.keys() or 'default_code' in vals.keys()):
+            return super(Product, self).write(vals)
+
+        # 1- RECEIVE UN UPDATE FROM TEKKEYS
+        if 'create_by' in vals.keys():
+            # GET BRAND, COMPANY AND CATEGORY ID FROM THE NAME GIVEN IN VALS
+            if 'brand' in vals:
+                vals['brand'] = self.create_brand(vals['brand']).id
+            if 'categ_id' in vals:
+                vals['categ_id'] = self.create_category(vals['categ_id']).id
+            if 'company_id' in vals:
+                vals['company_id'] = request.env['res.company'].search([('name', '=', vals['company_id'])]).id
+
+            # SELECT PRODUCT TYPE FROM THE DATA GIVEN IN VALS
+            if 'detailed_type' in vals:
+                if vals['detailed_type'] in ["storable product", "product"]:
+                    detailed_type = "product"
+                elif vals['detailed_type'] == "service":
+                    detailed_type = "service"
+                elif vals['detailed_type'] == "consumable":
+                    detailed_type = "consu"
+
+            # UPDATE CHARACTERISTICS LIST
+            attributes = [(5, 0, 0)]
+            for attr in vals['attribute'][0][2]:
+                attribute_items = [0, 0]
+                print(attr['attribute_id'])
+                print(attr['value_ids'])
+                attr_id = self.create_attribute(attr['attribute_id']).id
+                values_list = []
+                for value in attr['value_ids']:
+                    attr_value_id = self.create_attribute_value(value, attr_id).id
+                    values_list.append(attr_value_id)
+                print(values_list)
+
+                attribute_items.append({
+                    'attribute_id': attr_id,
+                    'value_ids': values_list
+                })
+
+                attributes.append(attribute_items)
+            vals['attribute_line_ids'] = attributes
+            vals.pop('attribute')
+            print(vals['attribute_line_ids'])
+
+
+            # existing_attribute_list = []
+            # for attribute in self.attribute_line_ids:
+            #
+            #     values_list = []
+            #     for value in attribute.value_ids:
+            #         values_list.append(value.name)
+            #     existing_attribute_list.append({
+            #         'attribute_id': attribute.attribute_id.name,
+            #         'value_ids': values_list
+            #     })
+            #
+            # attributes = [5, 0, 0]
+            # new_attrs_list = []
+            # my_list = existing_attribute_list
+            # for i in range(len(vals['attribute'][0][2])):
+            #     for exist_attr in existing_attribute_list:
+            #         attr = vals['attribute'][0][2][i]
+            #         if attr['attribute_id'] == exist_attr['attribute_id']:
+            #             attr_id = self.env['product.attribute'].search([("name", "ilike", attr['attribute_id'])])[0].id
+            #             if attr['value_ids'][0] not in exist_attr['value_ids']:
+            #                 attr_value_id = self.create_attribute_value(attr['value_ids'][0], attr_id).id
+            #                 exist_attr['value_ids'].append(attr_value_id)
+            #                 my_list = existing_attribute_list
+            #             vals['attribute'][0][2].pop(i)
+            #
+            #         else:
+            #             new_attrs_list.append(attr)
+            #
+            #     if new_attrs_list:
+            #         for item in new_attrs_list:
+            #             print("new found", item)
+            #             my_list.append(item)
+
+            rec = super(Product, self).write(vals)
+            print("API UPDATE", vals)
+            return rec
+        else:
+            return super(Product, self).write(vals)
 
     def create_brand(self, brand):
         # CHECK IF THE BRAND EXISTS
@@ -123,6 +228,26 @@ class Product(models.Model):
             new_category = self.env['product.category'].create({'name': category})
         return new_category
 
+    def create_attribute(self, attribute):
+        # CHECK IF THE attribute EXISTS
+        try:
+            attribute_id = self.env['product.attribute'].search([("name", "=", attribute)])[0]
+
+        # CREATE A NEW ONE IF NOT EXISTING
+        except:
+            attribute_id = self.env['product.attribute'].create({'name': attribute})
+
+        return attribute_id
+
+    def create_attribute_value(self, value, attr_id):
+        # CHECK IF THE attribute value EXISTS
+        try:
+            value_id = self.env['product.attribute.value'].search([("name", "=", value)])[0]
+        # CREATE A NEW ONE IF NOT EXISTING
+        except:
+            value_id = self.env['product.attribute.value'].create({'name': value, "attribute_id": attr_id})
+        return value_id
+
 
 class ProductBrand(models.Model):
     _name = 'product.brand'
@@ -136,6 +261,3 @@ class ProductBrand(models.Model):
         for field in self:
             res.append((field.id, field.name))
         return res
-
-
-
